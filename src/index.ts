72a1365285ceb5dc411dfb85f72ad420a1b9f065
AhmedMSalah87 <ahmed_salah45@hotmail.com>
2025-06-07 19:23:52 +0300

    first commit test homepage

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import authRouter from "./routes/auth.route.js";
 import cityRouter from "./routes/city.route.js";
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use("/api/users", authRouter);
 app.use("/api/cities", cityRouter);
 app.use("/api/users", cityRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000 and hi");
